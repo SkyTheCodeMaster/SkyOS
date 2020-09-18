b8ab876408c958c2919f7e76a2236bb243f278dc
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-09-18 09:49:12 -0400

    Update graphic.lua
    add "graphic.drawPixel(1,2,3)"

--- libraries/graphic.lua
+++ libraries/graphic.lua
@@ -1,6 +1,13 @@
 local graphic = {}
+
 -- Graphics library for SkyOS
+
 function graphic.drawFilledBox(arg1, arg2, arg3, arg4, arg5)
+--arg1 is top left x
+--arg2 is top left y
+--arg3 bottom right x
+--arg4 is bottom right y
+--arg5 is color (colors.black)
     currentColour = term.getBackgroundColor()
     currentTextColour = term.getTextColor()
  
@@ -11,6 +18,11 @@ function graphic.drawFilledBox(arg1, arg2, arg3, arg4, arg5)
 end
  
 function graphic.drawBox(arg1, arg2, arg3, arg4, arg5)
+--arg1 is top left x
+--arg2 is top left y
+--arg3 bottom right x
+--arg4 is bottom right y
+--arg5 is color (colors.black)
     currentColour = term.getBackgroundColor()
     currentTextColour = term.getTextColor()
  
@@ -18,6 +30,16 @@ function graphic.drawBox(arg1, arg2, arg3, arg4, arg5)
  
     term.setBackgroundColor(currentColour)
     term.setTextColor(currentTextColour)
+end
+
+function graphic.drawPixel(arg1, arg2, arg3)
+    currentColour = term.getBackgroundColor()
+    currentTextColour = term.getTextColor()
+    
+    paintutils.drawPixel(tonumber(arg1),tonumber(arg2),tonumber(arg3))
+    
+    term.setBackgroundColor(currentColour)
+    term.setTextColor(currentColour)
 end
  
 return graphic

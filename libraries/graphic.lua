1de5927ba6d2358ef291f516aca102868436392e
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-09-18 10:09:22 -0400

    Update graphic.lua
    add cursor positioning, should no longer put cursor in the latest drawn box.

--- libraries/graphic.lua
+++ libraries/graphic.lua
@@ -10,11 +10,15 @@ function graphic.drawFilledBox(arg1, arg2, arg3, arg4, arg5)
 --arg5 is color (colors.black)
     currentColour = term.getBackgroundColor()
     currentTextColour = term.getTextColor()
+    currentX, currentY = term.getCursorPos()
  
     paintutils.drawFilledBox(tonumber(arg1),tonumber(arg2),tonumber(arg3),tonumber(arg4),tonumber(arg5))
  
-    term.setBackgroundColor(currentColour)
+    
     term.setTextColor(currentTextColour)
+    term.setCursorPos(currentX,currentY-1)
+    term.setBackgroundColor(currentColour)
+    
 end
  
 function graphic.drawBox(arg1, arg2, arg3, arg4, arg5)
@@ -25,21 +29,30 @@ function graphic.drawBox(arg1, arg2, arg3, arg4, arg5)
 --arg5 is color (colors.black)
     currentColour = term.getBackgroundColor()
     currentTextColour = term.getTextColor()
+    currentX, currentY = term.getCursorPos()
  
     paintutils.drawBox(tonumber(arg1),tonumber(arg2),tonumber(arg3),tonumber(arg4),tonumber(arg5))
- 
-    term.setBackgroundColor(currentColour)
+
+    
     term.setTextColor(currentTextColour)
+    term.setCursorPos(currentX,currentY-1)
+    term.setBackgroundColor(currentColour)
 end
 
 function graphic.drawPixel(arg1, arg2, arg3)
+--arg1 is x
+--arg2 is y
+--arg3 is color (colors.black)
     currentColour = term.getBackgroundColor()
     currentTextColour = term.getTextColor()
+    currentX, currentY = term.getCursorPos()
     
     paintutils.drawPixel(tonumber(arg1),tonumber(arg2),tonumber(arg3))
     
-    term.setBackgroundColor(currentColour)
     term.setTextColor(currentColour)
+    term.setCursorPos(currentX,currentY-1)
+    term.setBackgroundColor(currentColour)
+    
 end
  
 return graphic

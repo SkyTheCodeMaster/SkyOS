8a0819f2d7835e64cdaa397325dfb75099410382
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-09-28 09:14:59 -0400

    Update timeManager.lua

--- customPrograms/timeManager.lua
+++ customPrograms/timeManager.lua
@@ -1,4 +1,8 @@
 local time = os.time()
-local formattedTime = textutils.formatTime(time, true)
-term.setCursorPos(22,20)
-term.write(formattedTime)
+while true do
+  local formattedTime = textutils.formatTime(time, true)
+  term.setCursorPos(22,20)
+  term.setBackgroundColour(colours.grey)
+  term.write(formattedTime)
+  term.setBackground(colours.black)
+end

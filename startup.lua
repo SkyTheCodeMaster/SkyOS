2f8ac014f6df3578ca006d5991153fdc474aeef9
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-09-29 20:25:24 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -44,6 +44,12 @@ while true do
   term.setCursorPos(22,20)
   term.write("     ")
   drawTime(22,20,128,256)
+  local event,pressedKey = os.pullEvent()
+  if event == "key" then
+      if pressedKey == 18
+          break
+      end
+  end
   sleep()
 end
 

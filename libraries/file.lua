a1bb108f29f07feeb9781c8c817cd5802a4e1969
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-10-01 12:49:28 -0400

    Update file.lua

--- libraries/file.lua
+++ libraries/file.lua
@@ -44,5 +44,39 @@ function file.loadGrpLines(path)
   end
   grpFile.close()
 end
+
+function loadAppGraphics(graphicPath,settingsPath,appName)
+  local graphicFile = fs.open(graphicPath,"r")
+  local settingsFile = fs.open(settingsPath,"r")
+  local x,y
+  for i = 1,file.countLines(settingsPath),1 do
+    local settingsLines = settingsFile.readLine()
+    local settingsTable = file.split(settingsLines,",")
+    local app = settingsTable[1]
+    if app == appName then
+      x,y = settingsTable[2],settingsTable[3]
+    end
+  end
+  for i = 1,file.countLines(graphicPath),1 do
+    local grpLine = graphicFile.readLine()
+    local grpTable = file.split(grpLine,",")
+    local operation = grpTable[1]
+    local a = tonumber(grpTable[2])+x
+    local b = tonumber(grpTable[3])+y
+    if operation == "P" then
+      graphic.drawPixel(a,b,tonumber(grpTable[4]))
+    elseif operation == "B" then
+      local c = tonumber(grpTable[4])+x
+      local d = tonumber(grpTable[5])+y
+      graphic.drawBox(a,b,c,d,tonumber(grpTable[6]))
+    elseif operation == "F" then
+      graphic.drawFilledBox(a,b,c,d,tonumber(grpTable[6]))
+    elseif operation == "L" then
+      graphic.drawLine(a,b,c,d,tonumber(grpTable[6]))
+    elseif operation == "TEXT" then
+      graphic.drawText(a,b,grpTable[4],grpTable[5],grpTable[6])
+    end
+  end
+end
 return file
  

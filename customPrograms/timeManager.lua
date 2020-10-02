26b51326bc6cbc518db338882cd8ddd77a19bfc7
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-10-02 11:42:53 -0400

    Delete timeManager.lua

--- customPrograms/timeManager.lua
+++
@@ -1,8 +0,0 @@
-local time = os.time()
-while true do
-  local formattedTime = textutils.formatTime(time, true)
-  term.setCursorPos(22,20)
-  term.setBackgroundColour(colours.grey)
-  term.write(formattedTime)
-  term.setBackgroundColour(colours.black)
-end

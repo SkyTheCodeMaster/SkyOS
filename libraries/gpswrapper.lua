226d5271af62219b43d948a298b0c410852a292d
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-10-23 23:43:35 -0400

    Update gpswrapper.lua

--- libraries/gpswrapper.lua
+++ libraries/gpswrapper.lua
@@ -6,8 +6,10 @@ gpswrapper = {}
  
 function gpswrapper.gpslocate(timeout)
     if fs.exists("rom/apis/gps.lua") then
+        sLog.info("[gpsw] gps api exists, getting gps")
         return gps.locate(timeout)
     else
+        sLog.warn("[gpsw] gps api does not exist, returning 1,1,1")
         return 1,1,1
     end
 end

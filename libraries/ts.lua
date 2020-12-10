c8834df38b86b7ce9d6596f2d259fd7d78a0225e
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-10 12:06:32 -0500

    Update ts.lua

--- libraries/ts.lua
+++ libraries/ts.lua
@@ -7,6 +7,11 @@ modem.open(pcReturn)
 local ts = {}
  
 function ts.get(offset)
+  if not SkyOS.settings.timeServerEnabled do
+    local epoch = math.floor(os.epoch("utc") / 1000) + (3600 * timezone) 
+    local t = os.date("!*t", epoch)
+    return t,tostring(t.hour) .. ":" .. tostring(t.min) .. ":" .. tostring(t.sec),tostring(t.hour) .. ":" .. tostring(t.min)
+  end
   modem.transmit(tsSend,pcReturn,offset)
   local _,_,_,_,msg = os.pullEvent("modem_message")
   return msg

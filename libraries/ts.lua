77ec02cf346a1897e816a59bf0ffe65f6af6eb4e
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-13 16:30:17 -0500

    Update ts.lua

--- libraries/ts.lua
+++ libraries/ts.lua
@@ -8,7 +8,7 @@ local ts = {}
  
 function ts.get(offset)
   if not SkyOS.settings.timeServerEnabled then
-    local epoch = math.floor(os.epoch("utc") / 1000) + (3600 * timezone) 
+    local epoch = math.floor(os.epoch("utc") / 1000) + (3600 * SkyOS.settings.timeZone) 
     local t = os.date("!*t", epoch)
     return t,tostring(t.hour) .. ":" .. tostring(t.min) .. ":" .. tostring(t.sec),tostring(t.hour) .. ":" .. tostring(t.min)
   end

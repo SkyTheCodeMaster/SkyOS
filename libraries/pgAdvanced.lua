edfcacf1c62a98eddc181e09ed027b376c0be005
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-30 13:40:32 -0500

    Create pgAdvanced.lua

---
+++ libraries/pgAdvanced.lua
@@ -0,0 +1,38 @@
+local bars = {}
+
+local pga = {}
+
+function pga.create(barName,stepCount,stepNames,curStep)
+  curStep = curStep or 1
+  bars[barName] = { curStep, stepCount, stepNames }
+  
+  if curStep ~= 1 then
+    local bar = pg.get(barName)
+    local tOutput = bar[7]
+    local x,y = tOutput.getCursorPos()
+    
+    pg.updateStep(unpack(bars[barName]))
+    tOutput.setCursorPos(bar[3],bar[4]+1)
+    tOutput.write("("..tostring(curStep).."/"..tostring(stepCount)..") "..stepNames[curStep])
+    tOutout.setCursorPos(x,y)
+  end
+end
+
+function pga.update(barName,curStep)
+  if bars[barName] == nil then error("bar does not exist")
+  curStep = curStep or 1
+  local bar = pg.get(barName)
+  local tOutput = bar[7]
+  local x,y = tOutput.getCursorPos()
+  
+  pg.updateStep(unpack(bars[barName]))
+  tOutput.setCursorPos(bar[3],bar[4]+1)
+  tOutput.write("("..tostring(curStep).."/"..tostring(stepCount)..") "..stepNames[curStep])
+  tOutput.setCursorPos(x,y)
+end
+
+function pga.delete(barName)
+  bars[barName] = nil
+end
+
+return pga

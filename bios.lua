049efe4b965ef60919c0bc09956a09f4189b0c8d
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-29 19:34:55 -0500

    Update bios.lua

--- bios.lua
+++ bios.lua
@@ -1,9 +1,10 @@
 _G.paintutils = require("libraries.apis.paintutils")
 _G.file = require("libraries.file")
-_G.graphic = require("libraries.file")
+_G.graphic = require("libraries.graphic")
 _G.pg = require("libraries.progressBar")
 local graphic = require("libraries.graphic")
 local file = require("libraries.file")
+local completion = require("cc.shell.completion")
 local pg = require("libraries.progressBar")
 local x,y = term.getSize()
 local dbFile = "data/main.skydb"
@@ -11,6 +12,13 @@ local dbFile = "data/main.skydb"
 term.setBackgroundColour(colours.blue)
 term.setTextColour(colours.white)
 term.clear()
+ 
+-- Initialize completion for programs
+-- edit
+ 
+local complete = completion.build( {completion.file} )
+shell.setCompletionFunction("edit.lua",complete)
+ 
 -- load variables into global environment
 local dbHandle = fs.open(dbFile,"r")
 _G.SkyOS = {}
@@ -26,3 +34,5 @@ local biosBar = pg.new(3,3,55,colours.lime,colours.grey,"biosBar",nil,mainMon)
 graphic.drawBox(1,0,x,y-1,colours.white)
 -- begin loading bg processes
 shell.run("bg bg/timeserver.lua")
+shell.run("bg bg/gps host -67 61 7")
+shell.run("bg bg/turtleserver.lua")

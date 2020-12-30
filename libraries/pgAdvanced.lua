f88c9b2a3d3f84b6b17f00a30a615028f945289a
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-30 14:39:57 -0500

    Update pgAdvanced.lua

--- libraries/pgAdvanced.lua
+++ libraries/pgAdvanced.lua
@@ -25,11 +25,15 @@ function pga.update(barName,curStep)
   local bar = pg.get(barName)
   local tOutput = bar[7]
   local x,y = tOutput.getCursorPos()
+  local pgaBar = bars[barName]
+  local stepCount,stepNames = pgaBar[2].pgaBar[3]
   
   pg.updateStep(unpack(bars[barName]))
   tOutput.setCursorPos(bar[3],bar[4]+1)
   tOutput.write("("..tostring(curStep).."/"..tostring(stepCount)..") "..stepNames[curStep])
   tOutput.setCursorPos(x,y)
+  pgaBar = { curStep, stepCount, stepNames }
+  bars[barName] = pgaBar
 end
 
 function pga.delete(barName)

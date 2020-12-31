80f41efbb06cf3523acbe998db4af6060b87bbd2
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2020-12-30 21:23:57 -0500

    Update bios.lua

--- bios.lua
+++ bios.lua
@@ -2,17 +2,24 @@ _G.paintutils = require("libraries.apis.paintutils")
 _G.file = require("libraries.file")
 _G.graphic = require("libraries.graphic")
 _G.pg = require("libraries.progressBar")
+_G.pga = require("libraries.pgAdvanced")
 local graphic = require("libraries.graphic")
 local file = require("libraries.file")
 local completion = require("cc.shell.completion")
 local pg = require("libraries.progressBar")
+local pga = require("libraries.pgAdvanced")
 local x,y = term.getSize()
 local dbFile = "data/main.skydb"
+ 
+-- shell colours
+local bgColour = colours.blue
+local promptColour = colours.yellow
+local textColour = colours.white
  
 term.setBackgroundColour(colours.blue)
 term.setTextColour(colours.white)
 term.clear()
-
+ 
 -- Initialize completion for programs
 -- edit
  
@@ -30,39 +37,60 @@ local mainMon = peripheral.wrap(SkyOS.monitors.main)
 mainMon.setTextScale(0.5)
 file.loadGrpLines("images/bootSplash.skgrp",mainMon)
 local biosBar = pg.new(3,3,55,colours.lime,colours.grey,"biosBar",nil,mainMon)
- 
-graphic.drawBox(1,0,x,y-1,colours.white)
-
+graphic.drawBox(2,5,56,7,256,mainMon)
+local bgBar = pg.new(3,5,55,colours.lime,colours.grey,"bgBar",nil,mainMon)
 -- begin loading bg processes
+local biosSteps = {
+  "Loading Background Processes",
+  "Loading Shell               ",
+}
+ 
+local bgSteps = {
+  "Timeserver",
+  "GPS Server",
+  "Turtleserver",
+}
+ 
+local aBiosBar = pga.create(biosBar,#biosSteps,biosSteps,1)
+local aBG = pga.create(bgBar,#bgSteps,bgSteps,1)
+ 
+pga.update(aBiosBar,1)
+pga.update(aBG,1)
 shell.run("bg bg/timeserver.lua")
+pga.update(aBG,2)
 shell.run("bg bg/gps host -67 61 7")
+pga.update(aBG,3)
 shell.run("bg bg/turtleserver.lua")
-
+pg.update(bgBar,100)
+file.loadGrpLines("images/bgBarPatch.skgrp",mainMon)
 -- custom shell
-
+ 
+pga.update(aBiosBar,2)
+term.setCursorPos(1,1)
 term.setBackgroundColor(bgColour)
 term.setTextColour(promptColour)
 print(os.version())
 term.setTextColour(textColour)
-
+ 
 -- Read commands and execute them
 local tCommandHistory = {}
-while not bExit do
-  term.redirect(parentTerm)
+pg.update(biosBar,100)
+file.loadGrpLines("images/pgPatch.skgrp",mainMon)
+while true do
   term.setBackgroundColor(bgColour)
   term.setTextColour(promptColour)
   write(shell.dir() .. "> ")
   term.setTextColour(textColour)
  
   local sLine
-   if settings.get("shell.autocomplete") then
-     sLine = read(nil, tCommandHistory, shell.complete)
-   else
-     sLine = read(nil, tCommandHistory)
-   end
-   if sLine:match("%S") and tCommandHistory[#tCommandHistory] ~= sLine then
-     table.insert(tCommandHistory, sLine)
-   end
-     shell.run(sLine)
-   end
+  if settings.get("shell.autocomplete") then
+    sLine = read(nil, tCommandHistory, shell.complete)
+  else
+    sLine = read(nil, tCommandHistory)
+  end
+  if sLine:match("%S") and tCommandHistory[#tCommandHistory] ~= sLine then
+    table.insert(tCommandHistory, sLine)
+  else
+    shell.run(sLine)
+  end
 end

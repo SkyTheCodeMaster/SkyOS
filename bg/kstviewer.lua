0d23d7b41cdbda5fbe906a81ac82b7159b50aa59
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-01-04 20:27:09 -0500

    Update kstviewer.lua

--- bg/kstviewer.lua
+++ bg/kstviewer.lua
@@ -1,3 +1,8 @@
+local function cut(str,len,pad)
+  pad = pad or " "
+  return str:sub(1,len) .. pad:rep(len - #str)
+end
+ 
 local response,err = http.post("https://krist.ceriat.net/ws/start","{}")
 if not response then error(err) end
  
@@ -9,31 +14,46 @@ socket.receive()
 local msgID = math.random(1,1000)
  
 local mon = peripheral.wrap(SkyOS.monitors.krist)
-local bf = require("libraries.bigfontSC")
  
 file.loadGrpLines("images/krist.skgrp",mon)
  
 term.setBackgroundColour(colours.blue)
 term.clear()
- 
-while true do
-  msgID = msgID + 1
-  socket.send(textutils.serializeJSON({type = "address", id = msgID, address = SkyOS.extra.krist.address}))
-  local r = socket.receive()
-  local data = textutils.unserializeJSON(r)
-  --print(textutils.serialize(data))
-  if data.type ~= "keepalive" then
-    mon.setTextColour(colours.grey)
-    mon.setCursorPos(14,5)
-    mon.setBackgroundColour(colours.lightBlue)
-    mon.write(tostring(data.address.totalin))
-    mon.setCursorPos(14,7)
-    mon.write(tostring(data.address.address))
-    mon.setBackgroundColour(colours.blue)
-    mon.setCursorPos(14,6)
-    mon.write(tostring(data.address.totalout))
-    mon.setTextColour(colours.white)
-    bf.writeOn(mon,2,string.char(164) .. tostring(data.address.balance),8,10)
-    sleep()
+
+local function main()
+  while true do
+    msgID = msgID + 1
+    socket.send(textutils.serializeJSON({type = "address", id = msgID, address = SkyOS.extra.krist.address}))
+    local r = socket.receive()
+    local data = textutils.unserializeJSON(r)
+    --print(textutils.serialize(data))
+    if data.address then
+      mon.setTextColour(colours.grey)
+      mon.setCursorPos(14,5)
+      mon.setBackgroundColour(colours.lightBlue)
+      mon.write(tostring(data.address.totalin))
+      mon.setBackgroundColour(colours.blue)
+      mon.setCursorPos(14,3)
+      mon.write(tostring(data.address.address))
+      mon.setBackgroundColour(colours.blue)
+      mon.setCursorPos(14,6)
+      mon.write(tostring(data.address.totalout))
+      mon.setTextColour(colours.white)
+      bf.writeOn(mon,2,string.char(164) .. tostring(data.address.balance),8,10)
+      sleep()
+    end
+    msgID = msgID + 1
+    socket.send(textutils.serializeJSON({type="work",id=msgID}))
+    local r = socket.receive()
+    local data = textutils.unserializeJSON(r)
+    if data.work then
+      mon.setBackgroundColour(colours.lightBlue)
+      mon.setTextColour(colours.grey)
+      mon.setCursorPos(14,7)
+      mon.write(cut(tostring(data.work),10))
+      sleep()
+    end
   end
 end
+
+main()

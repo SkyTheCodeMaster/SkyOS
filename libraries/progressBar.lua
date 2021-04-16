074ae605074433f847a71e3821d735ba85b2ec82
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-04-16 11:06:56 -0400

    Update progressBar.lua

--- libraries/progressBar.lua
+++ libraries/progressBar.lua
@@ -1,77 +1,78 @@
-local progressBar = {}
+--- progress bar is an api for drawing & updating progress bars.
+-- @module[kind=misc] progressbar
 
-if not fs.exists("libraries/graphic.lua") then
-  local h,err = http.get("https://raw.githubusercontent.com/SkyTheCodeMaster/SkyOS/master/libraries/graphic.lua")
-  if not h then error(err) end
-  local f = fs.open("libraries/graphic.lua","w")
-  f.write(h.readAll())
-  f.close()
-  h.close()
+local function save()
+  local x,y = term.getCursorPos()
+  local tbl = {
+    fg = term.getTextColour(),
+    bg = term.getBackgroundColour(),
+    x = x,
+    y = y,
+  }
+  return tbl
 end
 
-local graphic = require("libraries.graphic")
-
-local bars = {}
-
--- fill or filled is a PERCENTAGE of how full the bar is.
-
-function progressBar.calcFill(len,fill)
-
-  if fill > 100 then fill = 100 end
-  local div = 100 / len
-  local result = math.floor((fill / div) + 0.5)
-  
-  return result
-
+local function restore(tbl)
+  term.setCursorPos(tbl.x,tbl.y)
+  term.setTextColour(tbl.fg)
+  term.setBackgroundColour(tbl.bg)
 end
 
-local function calcSteps(step,total)
-  return step/total
+local function dfb(x,y,w,h,col) -- draw filled box
+  local tbl = save()
+  paintutils.drawFilledBox(x,y,w,h,col)
+  restore(tbl)
 end
 
-function progressBar.updateStep(name,step,total)
-  local percent = calcSteps(step,total) * 100
-  progressBar.update(name,percent)
-end
-
-function progressBar.update(name,filled)
-  if not bars[name] then error("bar does not exist") end
-  local tableBar = bars[name]
-
-  local _,len,x,y,fg,bg,tOutput = tableBar[1],tableBar[2],tableBar[3],tableBar[4],tableBar[5],tableBar[6],tableBar[7]
-  local pixels = progressBar.calcFill(len,filled)
-
-  graphic.drawFilledBox(x,y,len,y,bg,tOutput) -- draw over the old progess bar - useful if progress goes backwards
-  graphic.drawFilledBox(x,y,pixels,y,fg,tOutput)
-  bars[name] = {filled, len, x, y, fg, bg, tOutput}
-end
-
-function progressBar.new(x,y,len,fg,bg,name,filled,tOutput)
-  tOutput = tOutput or term.current()
-  filled = filled or 0
-
-  graphic.drawFilledBox(x,y,len,y,bg,tOutput) -- draw the background of it
-
-  bars[name] = {filled, len, x, y, fg, bg, tOutput}
-
-  if filled ~= 0 then
-    progressBar.update(name,filled,tOutput) -- if `filled` variable is passed, fill the progress bar to that amount.
+local function update(bar,percent)
+  -- Calculate pixel requirements
+  if percent > 100 then fill = 100 end
+  local pixels = math.floor((percent / (100 / bar.w)) + 0.5) -- The math.floor + 0.5 acts as a rounding function.
+  -- percent / (100 / barWidth) calculates how many pixels should be filled in the bar
+  dfb(bar.x,bar.y,(bar.x+bar.w),(bar.y+bar.h),bar.bg)
+  if pixels ~= 0 then
+    dfb(bar.x,bar.y,(bar.x+pixels),(bar.y+bar.h),bar.fg)
   end
-
-  return name
+  return percent
 end
 
-function progressBar.getFill(name)
-  if not bars[name] then error("bar does not exist") end
+local bar = {} --- @type bar
+local mt = {["__index"] = bar}
 
-  local tableBar = bars[name]
-  
-  return tableBar[1]
+--- Update the bar to a percentage from 0 to 100.
+-- @tparam number percent Percentage of how full the bar is.
+function bar:update(percent)
+  self.fill = update(self,percent)
 end
 
-function progressBar.get(name)
-  if not bars[name] then error("bar does not exist") end
-  return bars[name]
+--- Redraw the bar, putting it overtop of whatever has been drawn since.
+function bar:redraw()
+  update(self,self.fill)
 end
 
-return progressBar
+--- Create a bar object
+-- @tparam number x X coordinate of the bar.
+-- @tparam number y Y coordinate of the bar.
+-- @tparam number w Width of the bar.
+-- @tparam number h Height of the bar.
+-- @tparam number fg The colour of the filled in bar.
+-- @tparam number bg The colour of the background of the bar.
+-- @tparam[opt] number fill The pre filled portion of the bar. Defaults to 0.
+local function create(x,y,w,h,fg,bg,fill)
+  fill = fill or 0
+  local bar = {
+    x = x,
+    y = y,
+    w = w,
+    h = h,
+    fg = fg,
+    bg = bg,
+    fill = fill,
+  }
+  if fill ~= 0 then update(bar,fill) end
+  return setmetatable(bar,mt)
+end
+
+return {
+  create = create,
+}

9beca8dd38d0df1c0ec2da8284c3760db3d158e0
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-04-16 11:06:05 -0400

    Update log.lua

--- libraries/log.lua
+++ libraries/log.lua
@@ -1,56 +1,55 @@
-local mLog
-local LFT = {} -- Log File Table
-local log = {}
+--- The log library is a library to store logs for various programs. It will keep the logs down to a certain size.
+-- @module[kind=skyos] log
 
-function log.new(logPath,logName)
-  LFT[tostring(logName)] = fs.open(logPath,"a")
+local function genTime()
+  local t = os.date("!*t")
+  local str = "["..tostring(t.hour)..":"..tostring(t.min)..":"..tostring(t.sec).."] "
+  return str
 end
 
-function log.save(logName)
-  if logName == nil then logName = mLog end
-  LFT[tostring(logName)].flush()
+local tbl = {} --- @type log
+local mt = {["__index"] = tbl}
+--- Save the log, writing all the written info to the file. 
+function tbl:save() 
+  self.fHandle.save()
+end
+--- Close the log, stopping further use.
+function tbl:close()
+  self.fHandle.close()
+end
+--- Writes an info line to the log.
+-- @tparam string info The info to write to the log.
+function tbl:info(info)
+  local time = genTime()
+  local str = time .. "[INFO]" .. info
+  self.fHandle.writeLine(str)
+end
+--- Writes a warn line to the log.
+-- @tparam string warn The warn to write to the log.
+function tbl:warn(warn)
+  local time = genTime()
+  local str = time .. "[INFO]" .. warn
+  self.fHandle.writeLine(str)
+end
+--- Writes an errorline to the log.
+-- @tparam string error The error to write to the log.
+function tbl:err(err)
+  local time = genTime()
+  local str = time .. "[INFO]" .. err
+  self.fHandle.writeLine(str)
+end
+--- Create returns a table of functions for writing to a log file.
+-- @tparam string file Path to the log file.
+-- @treturn table log Logwith functions to write to the log file.
+local function create(file)
+  local fHandle,err = fs.open(file,"w")
+  if not fHandle then return nil,err end
+  local tbl = {
+    fHandle = fHandle
+  }
+  return setmetatable(tbl,mt)
 end
 
-function log.close(logName)
-  if logName == nil then logName = mLog end
-  LFT[tostring(logName)].close()
-end
-
-function log.info(text,logName)
-  if logName == nil then logName = mLog end
-  t = os.date("*t")
-  curTime = "[" .. string.gsub(os.date("%x"),"/",":") .. ":" .. os.date("%X") .. "]"
-  LFT[tostring(logName)].writeLine(curTime.." [INFO] "..tostring(text))
-end
-
-function log.warn(text,logName)
-  if logName == nil then logName = mLog end
-  t = os.date("*t")
-  curTime = "[" .. string.gsub(os.date("%x"),"/",":") .. ":" .. os.date("%X") .. "]"
-  LFT[tostring(logName)].writeLine(curTime.." [WARN] "..tostring(text))
-end
-
-function log.error(errorID,errorInfo,logName)
-  if logName == nil then logName = mLog end
-  t = os.date("*t")
-  curTime = "[" .. string.gsub(os.date("%x"),"/",":") .. ":" .. os.date("%X") .. "]"
-  LFT[tostring(logName)].writeLine(curTime.." [ERROR] "..tostring(errorID)..":"..tostring(errorInfo))
-end
-
-function log.errorC(errorID,errorInfo,logName)
-  if logName == nil then logName = mLog end
-  log.error(errorID,errorInfo,logName)
-  log.save(logName)
-  log.close(logName)
-  error(tostring(errorInfo).." See log for more")
-end
-
-function log.setMain(logName)
-  mLog = logName
-end
-
-function log.activeLogs()
-  return LFT
-end
-
-return log
+return {
+  create = create,
+}

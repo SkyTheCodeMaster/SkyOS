7426bd4cb70859a8a5c1fd07bc49f6cb39024e1d
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-04-29 23:26:08 -0400

    Update general.lua

--- settings/general.lua
+++ settings/general.lua
@@ -3,5 +3,7 @@ local gSet = {}
 gSet.timeZone = -5
 gSet.language = "en"
 gSet.timeServerEnabled = false
+gSet.desktopImg = "graphics/background/default.skimg"
+gSet.taskbarImg = "graphics/taskbar.skimg"
 
 return gSet
